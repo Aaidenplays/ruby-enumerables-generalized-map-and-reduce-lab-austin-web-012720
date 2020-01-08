c877d61d082079a9e65830f1b6ed5bd7d6d64c04
aaiden Nicolas Witten <aaidenplays@yahoo.com>
2020-01-08 04:37:41 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,7 +2,7 @@ def map(source_array)
   result = []
   counter = 0 
   while counter < source_array.size 
-    result << source_array[counter] * -1
+    result << (source_array[counter] * -1)
     counter += 1
   end
   yield result
